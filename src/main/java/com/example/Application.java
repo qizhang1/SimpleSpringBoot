7164f8f64aa567d12f85e336c08d0c4a8ad00b30
Qi.Zhang <qi.zhang@concur.com>
2015-10-30 23:50:07 -0700

    add swagger ui

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -8,14 +8,66 @@
  */
 
 package com.example;
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import com.fasterxml.classmate.TypeResolver;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {HelloController.class})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Autowired
+    private TypeResolver typeResolver;
+
+    
+    @Bean
+    public Docket petApi() {
+      return new Docket(DocumentationType.SWAGGER_2)
+          .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+          .pathMapping("/")
+          .directModelSubstitute(LocalDate.class, String.class)
+          .genericModelSubstitutes(ResponseEntity.class)
+          .alternateTypeRules(
+              newRule(typeResolver.resolve(DeferredResult.class,
+                      typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                      typeResolver.resolve(WildcardType.class)))
+          .useDefaultResponseMessages(false)
+          // override the 500 error code for all GETs
+          .globalResponseMessage(RequestMethod.GET,
+              newArrayList(new ResponseMessageBuilder()
+                  .code(500)
+                  .message("500 message")
+                  .responseModel(new ModelRef("Error"))
+                  .build()));
     }
 
 }
